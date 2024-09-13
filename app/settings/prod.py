a79c857d96e22e19c5f566c06777cea2b67fbc2b
elleon003 <noelle@ygitc.com>
2024-09-13 18:42:48 -0400

    Remove HTTPS settings from prod.py

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -10,10 +10,10 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
 # HTTPS settings
-SECURE_SSL_REDIRECT = True
-SECURE_HSTS_SECONDS = 31536000  # 1 year
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
+# SECURE_SSL_REDIRECT = True
+# SECURE_HSTS_SECONDS = 31536000  # 1 year
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
 
 # Email configuration (replace with your actual email settings)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
