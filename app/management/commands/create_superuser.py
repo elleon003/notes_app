2a92eeba842ac414daef1afddef45b6d142b7bbd
elleon003 <noelle@ygitc.com>
2024-09-13 22:36:10 -0400

    part two

---
+++ app/management/commands/create_superuser.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from decouple import config
+
+
+class Command(BaseCommand):
+    help = 'Creates a superuser if none exist'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        if User.objects.filter(is_superuser=True).count() == 0:
+            # username = config('DJANGO_SUPERUSER_USERNAME')
+            email = config('DJANGO_SUPERUSER_EMAIL')
+            password = config('DJANGO_SUPERUSER_PASSWORD')
+            
+            User.objects.create_superuser(email=email, password=password)
+            
+            self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" was created'))
+        else:
+            self.stdout.write(self.style.SUCCESS('Superuser already exists.'))
