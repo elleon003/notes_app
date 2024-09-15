662c55b42c3e3d405de1d9f2405309a9d438ffba
elleon003 <noelle@ygitc.com>
2024-09-14 20:28:27 -0400

    Added --clear to collectstatic

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ cd /app
 # Ensure proper permissions
 chmod -R 755 /app/staticfiles
 
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 python manage.py migrate --noinput
 python manage.py create_default_superuser
 
