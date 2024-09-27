12c26b4484791e8e26a9a9351000cc1ead568af7
elleon003 <noelle@ygitc.com>
2024-09-27 01:46:59 -0400

    Fix Login

--- users/forms.py
+++ users/forms.py
@@ -10,28 +10,8 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ('email', )
 
 class CustomAuthenticationForm(AuthenticationForm):
-    cf_turnstile_response = forms.CharField(required=False)
-
     def clean(self):
         cleaned_data = super().clean()
-        cf_turnstile_response = cleaned_data.get('cf_turnstile_response')
-
-        if not cf_turnstile_response:
-            raise forms.ValidationError('Turnstile validation failed. Please try again.')
-
-        # Verify Turnstile response
-        data = {
-            'secret': settings.TURNSTILE_SECRET,
-            'response': cf_turnstile_response,
-        }
-        response = requests.post('https://challenges.cloudflare.com/turnstile/v0/siteverify', data=data)
-        result = response.json()
-
-        if not result.get('success'):
-            raise forms.ValidationError('Turnstile validation failed. Please try again.')
-
-        if self.user_cache:
-            email = self.user_cache.email
         return cleaned_data
 
 class CustomUserChangeForm(UserChangeForm):
